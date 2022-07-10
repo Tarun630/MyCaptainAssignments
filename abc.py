a012857f56427699ea5b4d278c5b2a9266eac673
Tarun630 <92390220+Tarun630@users.noreply.github.com>
2022-07-10 22:53:43 +0530

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,7 @@
+from math import pi
+r=float(input("Input the radius of the circle : "))
+Area=str(pi* r**2);
+print("The area of the circle with radius " + str(r) + " is: " + Area)
+
+filename = input("Input the Filename: ")
+print ("The extension of the file is : 'python'")
